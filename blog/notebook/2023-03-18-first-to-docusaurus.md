995c49cfacb030d8b44e465db0d7ab094533c705
panda8z <panda8@126.com>
2023-03-18 17:20:45 +0800

    deploy with github action

---
+++ blog/notebook/2023-03-18-first-to-docusaurus.md
@@ -0,0 +1,42 @@
+---
+slug: first-to-docusaurus
+title: First to Docusaurus
+authors: [panda8z]
+tags: [How-to, docusaurus]
+---
+
+## how to 
+
+
+added 1096 packages, and audited 1097 packages in 4m
+
+201 packages are looking for funding
+  run `npm fund` for details
+
+22 vulnerabilities (4 moderate, 18 high)
+
+Some issues need review, and may require choosing
+a different dependency.
+
+Run `npm audit` for details.
+[SUCCESS] Created docs.
+[INFO] Inside that directory, you can run several commands:
+
+  `npm start`
+    Starts the development server.
+
+  `npm run build`
+    Bundles your website into static files for production.
+
+  `npm run serve`
+    Serves the built website locally.
+
+  `npm deploy`
+    Publishes the website to GitHub pages.
+
+We recommend that you begin by typing:
+
+  `cd docs`
+  `npm start`
+
+Happy building awesome websites!
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,21 +6,21 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DDY Docs',
+  tagline: 'DDY are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://panda8z.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'panda8z', // Usually your GitHub org/user name.
+  projectName: 'panda8z.github.io', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
@@ -41,15 +41,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
